946410be8d8857b55c030e0419013760bc001511
Miguel Lumapat <lamppot@protonmail.com>
2020-11-09 23:02:33 -0500

    Create ChessCoord and ChessColor

--- src/Chess/Board.hs
+++ src/Chess/Board.hs
@@ -2,6 +2,7 @@
 
 module Chess.Board
     ( Board (..)
+    , ChessCoord (..)
     , File (..)
     , Piece (..)
     , Rank (..)
@@ -15,14 +16,20 @@ import Control.Applicative
 import qualified Data.Vector as DV
 import Data.Ix (range)
 
-data ChessColor = Black
-                | White
-                deriving (Eq, Show)
+data ChessColor = White
+                | Black
+                deriving (Enum, Eq, Ord, Show)
 
+opposingChessColor :: ChessColor -> ChessColor
+opposingChessColor Black = White
+opposingChessColor White = Black
+
+-- TODO: Rename to ChessPiece
 data Piece = Piece ChessColor PieceClass
            | Open
            deriving (Eq)
 
+-- TODO: Rename to ChessPieceClass
 data PieceClass = King
                 | Queen
                 | Rook
@@ -66,14 +73,27 @@ instance Enum PieceClass where
 data Board = Board
     { boardMaxRanks :: Int
     , boardMaxFiles :: Int
-    , boardSquares :: DV.Vector (DV.Vector Piece)
+    , boardSquares :: DV.Vector (DV.Vector Square)
     } deriving (Eq, Show)
 
 maxRanks = 8
 maxFiles = 8
 
+newtype File = File Int deriving (Enum, Eq, Num, Ord, Show)
+newtype Rank = Rank Int deriving (Enum, Eq, Num, Ord, Show)
+
+data ChessCoord = ChessCoord
+    { boardRank :: Rank
+    , boardFile :: File
+    } deriving (Eq, Ord, Show)
+
+data Square = Square
+    { squareColor :: ChessColor
+    , squarePiece :: Piece
+    } deriving (Eq, Show)
+
 cleanBoard :: Board
-cleanBoard = Board maxRanks maxFiles $ DV.fromList
+cleanBoard = Board maxRanks maxFiles $ DV.fromList $ alternatingColorsOf
     [ royalRow Black
     , peasantRow Black
     , blankRow
@@ -82,7 +102,13 @@ cleanBoard = Board maxRanks maxFiles $ DV.fromList
     , blankRow
     , peasantRow White
     , royalRow White
-    ]
+    ] where alternatingColorsOf = zipWith alternateColoredRow (cycle [White, Black])
+
+alternateColoredRow :: ChessColor -> DV.Vector Piece -> DV.Vector Square
+alternateColoredRow color pieces = DV.zipWith Square colors pieces where
+    colors = DV.generate maxFiles alternatingColors
+    alternatingColors ix | even ix   = color
+                         | otherwise = opposingChessColor color
 
 blankRow :: DV.Vector Piece
 blankRow = DV.replicate maxFiles Open
@@ -93,35 +119,27 @@ peasantRow color = DV.replicate maxFiles (Piece color Pawn)
 royalRow :: ChessColor -> DV.Vector Piece
 royalRow color = DV.fromList $ (Piece color) <$> [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
 
-newtype File = File Int deriving (Enum, Eq, Num, Ord, Show)
-newtype Rank = Rank Int deriving (Enum, Eq, Num, Ord, Show)
+-- square :: Int -> Int -> Square
+-- square rank file = Square (Rank rank) (File file)
 
-data Square = Square
-    { boardRank :: Rank
-    , boardFile :: File
-    } deriving (Eq, Ord, Show)
-
-square :: Int -> Int -> Square
-square rank file = Square (Rank rank) (File file)
-
-squareIndices :: Square -> (Int, Int)
-squareIndices (Square (Rank row) (File col)) = (row, col)
+squareIndices :: ChessCoord -> (Int, Int)
+squareIndices (ChessCoord (Rank row) (File col)) = (row, col)
 
 -- TODO: Use lens or another means to update??
 -- Switch a square's file and rank
-squareUp :: Square -> (Rank -> Rank) -> (File -> File) -> Square
-squareUp s rankT fileT = Square (rankT $ boardRank s) (fileT $ boardFile s)
+squareUp :: ChessCoord -> (Rank -> Rank) -> (File -> File) -> ChessCoord
+squareUp c rankT fileT = ChessCoord (rankT $ boardRank c) (fileT $ boardFile c)
 
-rankUp :: Square -> (Rank -> Rank) -> Square
-rankUp sq rankT = squareUp sq rankT id
+rankUp :: ChessCoord -> (Rank -> Rank) -> ChessCoord
+rankUp c rankT = squareUp c rankT id
 
-fileUp :: Square -> (File -> File) -> Square
-fileUp sq fileT = squareUp sq id fileT
+fileUp :: ChessCoord -> (File -> File) -> ChessCoord
+fileUp c fileT = squareUp c id fileT
 
 -- TODO: Docs
 -- Rider squares are squares going in multiple directions
-riderSquares :: Int -> Int -> Square -> [Square]
-riderSquares r f s = (uncurry Square) <$> (drop 1 $ zip ranks files)
+riderSquares :: Int -> Int -> ChessCoord -> [ChessCoord]
+riderSquares r f s = drop 1 $ zipWith ChessCoord ranks files
     where rankInc = Rank r
           fileInc = File f
           startingRank = boardRank s
@@ -129,28 +147,28 @@ riderSquares r f s = (uncurry Square) <$> (drop 1 $ zip ranks files)
           ranks = [startingRank, startingRank + rankInc..]
           files = [startingFile, startingFile + fileInc..]
 
-inBounds :: Board -> Square -> Bool
-inBounds b s | (fromEnum $ boardFile s) < 0                 = False
-             | (fromEnum $ boardFile s) > (boardMaxFiles b) = False
-             | (fromEnum $ boardRank s) < 0                 = False
-             | (fromEnum $ boardRank s) > (boardMaxRanks b) = False
+inBounds :: Board -> ChessCoord -> Bool
+inBounds b c | (fromEnum $ boardFile c) < 0                 = False
+             | (fromEnum $ boardFile c) > (boardMaxFiles b) = False
+             | (fromEnum $ boardRank c) < 0                 = False
+             | (fromEnum $ boardRank c) > (boardMaxRanks b) = False
              | otherwise                                    = True
 
-open :: Board -> Square -> Bool
-open b s = ((boardSquares b) DV.! col DV.! row) == Open
-    where (col, row) = squareIndices s
+open :: Board -> ChessCoord -> Bool
+open b c = squarePiece ((boardSquares b) DV.! row DV.! col) == Open
+    where (row, col) = squareIndices c
 
-pieceAt :: Board -> Square -> Maybe Piece
-pieceAt b s | not $ inBounds b s = Nothing
-            | otherwise          = Just $ (boardSquares b) DV.! (fromEnum $ boardRank s) DV.! (fromEnum $ boardFile s)
+pieceAt :: Board -> ChessCoord -> Maybe Piece
+pieceAt b c | not $ inBounds b c = Nothing
+            | otherwise          = Just $ squarePiece $ (boardSquares b) DV.! (fromEnum $ boardRank c) DV.! (fromEnum $ boardFile c)
 
 
-moves :: Board -> Square -> Maybe [Square]
-moves b s = do (Piece _ pieceClass) <- pieceAt b s
-               return $ pieceMove pieceClass b s
+moves :: Board -> ChessCoord -> Maybe [ChessCoord]
+moves b c = do (Piece _ pieceClass) <- pieceAt b c
+               return $ pieceMove pieceClass b c
 
-pieceMove :: PieceClass -> Board -> Square -> [Square]
-pieceMove King b s = ((uncurry Square) . ff) <$> (filter (/= (0,0)) $ range ((-1, -1), (1,1)))
+pieceMove :: PieceClass -> Board -> ChessCoord -> [ChessCoord]
+pieceMove King b s = filter (inBounds b) $ ((uncurry ChessCoord) . ff) <$> (filter (/= (0,0)) $ range ((-1, -1), (1,1)))
     where f = (+ (boardRank s)) . Rank
           f' = (+ (boardFile s)) . File
           ff (rank, file) = (f rank, f' file)
@@ -171,7 +189,7 @@ pieceMove Bishop b s = ne ++ nw ++ se++ sw
 
 pieceMove Queen b s = (pieceMove Rook b s) ++ (pieceMove Bishop b s)
 
-pieceMove Knight b s = newSq <$> mvmts
+pieceMove Knight b s = filter (inBounds b) $ newSq <$> mvmts
     where f op (a,b) (c,d) = (op a c, op b d)
           mvmts = (f (*)) <$> [(-1,-1), (-1,1), (1,-1), (1,1)] <*> [(1,2), (2,1)]
           newSq (a, b) = squareUp s (+ Rank b) (+ File a)
