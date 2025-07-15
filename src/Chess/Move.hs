d03e377f318b87c9fd44df8717679cbf56989e16
Miguel Lumapat <lamppot@protonmail.com>
2025-07-15 00:10:47 -0400

    Parse other piece types for moves

--- src/Chess/Move.hs
+++ src/Chess/Move.hs
@@ -40,12 +40,28 @@ parseChessMove ::
   ChessColor ->
   LT.Parser ChessMove
 parseChessMove color =
-  ( PieceMove (ChessPiece color ChessPawn)
-      <$> parseChessPosition
+  ( PieceMove <$> (ChessPiece color <$> parsePieceType)
+      <*> parseChessPosition
       <*> return MoveUnrestricted
   )
     <|> (PieceCapture (ChessPiece color ChessPawn) <$> (parseCaptures *> parseChessPosition) <*> return MoveUnrestricted)
     <* LT.endOfInput
+
+parsePieceType :: LT.Parser ChessPieceType
+parsePieceType =
+  LT.choice
+    ( mapPiece
+        <$> [ ('B', ChessBishop),
+              ('N', ChessKnight),
+              ('K', ChessKing),
+              ('R', ChessRook),
+              ('Q', ChessQueen)
+            ]
+    )
+    <|> return ChessPawn
+  where
+    mapPiece :: (Char, ChessPieceType) -> LT.Parser ChessPieceType
+    mapPiece (c, p) = LT.char c $> p
 
 parseCaptures :: LT.Parser Char
 parseCaptures = LT.char 'x'
